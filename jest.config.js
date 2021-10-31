735594c2c8589063c921a62c1733e5d1174ab4e6
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-31 21:39:23 +0700

    test: setup jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  roots: [path.join(__dirname, "./server/src")],
+  rootDir: path.join(__dirname, "./"),
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**"],
+  moduleDirectories: [
+    "node_modules",
+    path.join(__dirname, "./test"),
+    path.join(__dirname, "./src"),
+  ],
+  coverageDirectory: path.join(__dirname, "./coverage/collective"),
+  collectCoverageFrom: ["**/src/**/*.js"],
+  coveragePathIgnorePatterns: [".*/__tests__/.*"],
+  watchPlugins: [
+    require.resolve("jest-watch-typeahead/filename"),
+    require.resolve("jest-watch-typeahead/testname"),
+  ],
+  setupFilesAfterEnv: [require.resolve("./test/setup-env")],
+};
---
+++ test/setup-env.js
@@ -0,0 +1,3 @@
+const { join } = require("path");
+
+require("dotenv").config({ path: join(__dirname, "../.env.test") });
